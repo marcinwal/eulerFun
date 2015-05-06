c74b87d209a866d08854fce7b058e15eddce2676
marcin walendzik <marcin.walendzik@gmail.com>
2015-05-06 10:30:18 +0100

    003 added

---
+++ euler003.py
@@ -0,0 +1,34 @@
+
+
+def findPrimeDivisorMax(number):
+  largest = 0;
+  for i in range(2,number):
+    if (number % i == 0):
+      isPrime = True
+      for j in range(2,i):
+        if(i % j == 0):
+          isPrime = False
+          break
+      if (isPrime):
+        largest = i;
+  return largest
+
+# any int can be decomposed into primes multiplications or is a prime number!
+def findPrimeDivisorMaxBest(number):
+  largest = 0
+  counter = 2
+  numb = number
+  while (counter * counter <= numb):
+    print counter
+    if(numb % counter == 0):
+      numb = numb/counter
+      largest = counter
+    else:
+      counter += 1
+  if (numb > largest):
+    largest = numb
+  return largest
+
+print findPrimeDivisorMaxBest(600851475143)
+print findPrimeDivisorMax(1111)
+print findPrimeDivisorMaxBest(1111)
