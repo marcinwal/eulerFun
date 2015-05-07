c7f0cd60b6ea58eb4fc4b9d8ef4e7ac7ee73eb60
marcin walendzik <marcin.walendzik@gmail.com>
2015-05-07 13:04:08 +0100

    analytical solution

--- euler006.py
+++ euler006.py
@@ -3,8 +3,12 @@ def answer(number):
   for i in range(1,number):
     for j in range(i+1,number+1):
       sum += 2*i*j
-      print i,j
   return sum
 
+def analytical(number):
+  sum = number *(number + 1) / 2
+  squared = (number * (number + 1) * (2 * number + 1)) / 6
+  return sum * sum - squared
 
 print answer(100)
+print analytical(100)
